f55e45bf7d5d8b61b840c65fbad8024b761dc6f6
Arthur Fleischman <cabralfle@gmail.com>
2021-09-17 03:09:18 -0300

    fixing print in error handler

--- lib/siga_web/auth/auth_error_handler.ex
+++ lib/siga_web/auth/auth_error_handler.ex
@@ -5,7 +5,8 @@ defmodule SigaWeb.Guardian.AuthErrorHandler do
 
   @impl Guardian.Plug.ErrorHandler
   def auth_error(conn, {type, reason}, _opts) do
-    IO.puts(%{message: to_string(type), reason: to_string(reason)})
+    IO.puts(to_string(type))
+    IO.puts(to_string(reason)})
 
     conn
     |> put_status(:bad_request)
