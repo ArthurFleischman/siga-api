f7e34f7e0bfe4e9d857e5f450cf91bcd779c99b5
Arthur Fleischman <cabralfle@gmail.com>
2021-09-17 01:37:42 -0300

    update error handler

--- lib/siga_web/auth/auth_error_handler.ex
+++ lib/siga_web/auth/auth_error_handler.ex
@@ -4,9 +4,9 @@ defmodule SigaWeb.Guardian.AuthErrorHandler do
   import Plug.Conn
 
   @impl Guardian.Plug.ErrorHandler
-  def auth_error(conn, {type, _reason}, _opts) do
+  def auth_error(conn, {type, reason}, _opts) do
     conn
     |> put_status(:bad_request)
-    |> json(%{message: to_string(type)})
+    |> json(%{message: to_string(type), reason: to_string(reason)})
   end
 end
